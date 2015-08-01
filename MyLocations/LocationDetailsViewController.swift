a01e7cfabcb1733cd1bb59fdd290581a5e85d998
wingleungchoi <wingleungchoi@gmail.com>
2015-08-01 19:00:43 +0800

    able to show details in taglocation

---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,81 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Wing LeungCHOI on 1/8/15.
+//  Copyright (c) 2015 WingLeung CHOI. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    return formatter
+}()
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    
+    @IBAction func done() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func cancel() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2{
+            addressLabel.frame.size = CGSize(width: view.bounds.size.width - 115, height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 15
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = stringFromPlacemark(placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+    
+        dateLabel.text = formatDate(NSDate())
+    }
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String{
+        return
+        "\(placemark.subThoroughfare) \(placemark.thoroughfare), " +
+        "\(placemark.locality), " +
+        "\(placemark.administrativeArea) \(placemark.postalCode), " +
+        "\(placemark.country)"
+    }
+    
+    func formatDate(date: NSDate) -> String {
+        return dateFormatter.stringFromDate(date)
+    }
+}
